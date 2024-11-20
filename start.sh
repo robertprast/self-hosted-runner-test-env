e3be6530e1799eda7e5a225a60470f808732a87d
--replace-all <rmp452@nyu.edu>
2024-11-20 00:39:27 -0500

    Use ephermeral runners

--- start.sh
+++ start.sh
@@ -10,15 +10,18 @@ REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: a
 cd /home/docker/actions-runner
 
 echo "Running config"
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --ephemeral
 
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN} -ephemeral
+    ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 ./run.sh & wait $!
+
+
+while true; do ls -laR ./  sleep 1; done
